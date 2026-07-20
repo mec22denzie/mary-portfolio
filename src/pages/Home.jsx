60a180d641066082ff2471944019d12e65d9484b
Mary <mec22denzie@yahoo.com>
2026-07-20 13:08:31 -0400

    Fix unused Projects import

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import Hero from "../components/Hero";
 import About from "../components/About";
-import Projects from "../components/Projects/Projects";
+//import Projects from "../components/Projects/Projects";
 import Skills from "../components/Skills";
 import Contact from "../components/Contact";
 
